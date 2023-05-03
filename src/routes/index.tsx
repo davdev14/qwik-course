f0a7b7a82a4721ea1f3cdbd1eee15bcfc2aca440
davdev14 <105063415+davdev14@users.noreply.github.com>
2023-05-03 16:52:28 +0200

    first qwik component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,2 +1,10 @@
+import { component$ } from "@builder.io/qwik"
 
+export default component$(() => {
 
+  console.log('initializing hello world component');
+  
+  return (
+    <h1>Welcom to this Qwik course v2</h1>
+  )
+})
