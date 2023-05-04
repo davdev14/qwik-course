d3590715b969db97fca4c703062c73affd0e9440
davdev14 <105063415+davdev14@users.noreply.github.com>
2023-05-04 04:36:23 +0200

    styles

--- src/components/hello-message/hello-message.tsx
+++ src/components/hello-message/hello-message.tsx
@@ -1,21 +1,43 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, PropFunction, useStylesScoped$ } from "@builder.io/qwik";
+import styles from './hello-message.css?inline'
 
 interface HelloMessageProps {
   message: string;
   courseVersion?: number;
+  showButton: boolean
+  onShowMessage: PropFunction<(message: string) => void>
 }
 
 
 export const HelloMessage = component$((props: HelloMessageProps) => {
 
-  const {message, courseVersion} = props
+  useStylesScoped$(styles)
+
+  const {message, courseVersion, onShowMessage, showButton} = props
+
+  const cssClasses = ['hello-message']
+  if (courseVersion === 1) {
+    cssClasses.push('highlighted')
+  }
+  const customStyles = (courseVersion === 2) ? {
+    color: 'red',
+    textDecoration: 'underline'
+  } : {}
 
   return (
     <div class="container">
       {
-        <h1>
+        <>
+        <h1 class={cssClasses} style={customStyles}>
           {message} - {courseVersion}
         </h1>
+
+        {
+          showButton && (<button onClick$={ () => onShowMessage(message)}>show message</button>)
+        }
+
+        
+        </>
       }
     </div>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,28 @@
-import { component$, $ } from "@builder.io/qwik"
+import { component$, $ } from "@builder.io/qwik";
 import { HelloMessage } from "~/components/hello-message/hello-message";
 
 export default component$(() => {
+  console.log("initializing hello world component");
 
-  console.log('initializing hello world component');
-  
-  const sayHello = $( () => {
-    alert("hello world")
-  })
+  const sayHello = $(() => {
+    alert("hello world");
+  });
+
+  const onShowMessageClicked = $((message: string) => alert(message))
+
+  const messages = [
+    "hello world",
+    "welcome to this Qwik course",
+    "learn Qwik framework"
+  ]
 
   return (
     <>
-      <HelloMessage courseVersion={1} message="hello world!" />
-      <HelloMessage courseVersion={1} message="hello world!"/>
-      <HelloMessage message="hello world!"/>
+      { messages.map((message, index) => (
+        <HelloMessage key={index} message={message} showButton={true} onShowMessage={onShowMessageClicked} courseVersion={index} />
+      )) }
 
       <button onClick$={sayHello}>say hello</button>
     </>
-  )
-})
+  );
+});
